77af7fb8418826256e6db66f3daf2a1413ced742
Daniel Barnes <dabarnes2b@gmail.com>
2025-06-07 10:49:46 +0900

    line deleted

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -21,7 +21,6 @@ return {
       require('telescope').load_extension('fzf')
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, { desc = "LSP Hover"})
-
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
