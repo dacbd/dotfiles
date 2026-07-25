5fe81f37852b34befd7daf438680284be36b39f0
Daniel Barnes <dabarnes2b@gmail.com>
2026-07-25 03:53:12 -0700

    add bootstrap script

---
+++ .dacbd/misc/bootstrap.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+brew tap hashicorp/tap
+brew install stow \
+    starship \
+    font-hack-nerd-font \
+    wget \
+    gum \
+    jq \
+    neovim \
+    luarocks \
+    tmux \
+    gnupg \
+    pinentry-mac \
+    cmake \
+    kind \
+    derailed/k9s/k9s \
+    podman \
+    hashicorp/tap/terraform \
+    azure-cli \
+    helm \
+    cmctl
+brew install --cask zedbrew install --cask zed
+brew install --cask ghostty
